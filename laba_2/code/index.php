48b2fb85af9bb878a4858616fc502d112fc7f31e
sonicccccccccccccccc <dedad411@gmail.com>
2024-03-07 01:13:33 +0200

    task 2 + screenshot

--- laba_2/code/index.php
+++ laba_2/code/index.php
@@ -1,4 +1,4 @@
-<?php
+<?php  //task 1
 /* Imagine a lot of code here */
 $very_bad_unclear_name = "15 chicken wings";
 
@@ -10,3 +10,16 @@ $order.= " from McDonald's with love ";
 
 // Don't change the line below
 echo "\nYour order is: $very_bad_unclear_name.";
+
+//task 2
+$a = 6;
+echo "\n$a";
+$b = 2;
+echo "\n$b";
+$c = $a * $b;
+echo "\n$c";
+
+$last_month = 1187.23;
+$this_month = 1089.98;
+$w = $last_month - $this_month;
+echo "\nНасколько больше я потратил в прошлом месяце, чем в этом месяце? На $w ";
