f8aea5a6b90a4ba19d9cd0407f15dd19b6e0838c
sonicccccccccccccccc <dedad411@gmail.com>
2024-03-17 00:24:16 +0200

    task 1a) + screenshot

--- laba_3/task 1-2/index.php
+++ laba_3/task 1-2/index.php
@@ -1,1 +1,10 @@
-<?php //task 1
+<?php //task 1 a)
+$str= 'abba adca abea aaaa abcd';
+$sample= '/a[a-z][a-z]a/ui';
+$matches= array();
+preg_match_all($sample,$str,$matches);
+foreach($matches[0] as $match)
+{
+    echo $match,"\n";
+}
+
