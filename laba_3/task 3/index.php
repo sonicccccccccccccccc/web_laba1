0e46521c5458191a87f0b6639e2e93afe22b6ae2
sonicccccccccccccccc <dedad411@gmail.com>
2024-03-26 01:49:14 +0200

    task 3 4/4 + screenshot

--- laba_3/task 3/index.php
+++ laba_3/task 3/index.php
@@ -7,29 +7,62 @@
     <title>BAZAR</title>
 </head>
 <body>
-    <form action="Save.php" method="post">
-        <label for="email">Email:</label>
-        <input type="email" name="email" required>
-        <label for="category">Category:</label>
-        <select name="category" required>
-            <?php
-            $path = "./categories/";
-            if (is_dir($path))
-            {
-                $folders = array_filter(glob($path . '*'), 'is_dir');
-                foreach ($folders as $folder)
+    <div id="form">
+        <form action="Save.php" method="post">
+            <label for="email">Email:</label>
+            <input type="email" name="email" required>
+            <label for="category">Category:</label>
+            <select name="category" required>
+                <?php
+                $path = "./categories/";
+                if (is_dir($path))
                 {
-                    $name = basename($folder);
-                    echo "<option value=\"$name\">$name</option>";
+                    $folders = array_filter(glob($path . '*'), 'is_dir');
+                    foreach ($folders as $folder)
+                    {
+                        $name = basename($folder);
+                        echo "<option value=\"$name\">$name</option>";
+                    }
+                }
+                ?>
+            </select>
+            <label for="title">Title:</label>
+            <input type="text" id="title" name="title" required>
+            <label for="description">Description:</label>
+            <textarea rows="5" name="description" id="description" required></textarea>
+            <input type="submit" value="Save">
+        </form>
+    </div>
+    <div id="table">
+        <table>
+            <thead>
+                <th>Email</th>
+                <th>Category</th>
+                <th>Title</th>
+                <th>Description</th>
+            </thead>
+            <?php
+            $categ = ['Cars', 'Electronics', 'Pets','Other'];
+            foreach ($categ as $category) {
+                $folder = "./categories/$category";
+                if (is_dir($folder)) {
+                    $files = scandir($folder);
+                    foreach ($files as $file) {
+                        if ($file != '.' && $file != '..') {
+                            $content = file_get_contents("$folder/$file");
+                            $znach = explode("\n", $content);
+                            echo "<tbody>";
+                            echo "<td>{$znach[0]}</td>";
+                            echo "<td>$category</td>";
+                            echo "<td>{$znach[1]}</td>";
+                            echo "<td>{$znach[2]}</td>";
+                            echo "</tbody>";
+                        }
+                    }
                 }
             }
             ?>
-        </select>
-        <label for="title">Title:</label>
-        <input type="text" id="title" name="title" required>
-        <label for="description">Description:</label>
-        <textarea rows="5" name="description" id="description" required></textarea>
-        <input type="submit" value="Save">
-    </form>
+        </table>
+    </div>
 </body>
 </html>
